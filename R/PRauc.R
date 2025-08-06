243d5388263317c3bc7412fdbbc7f5728a43652f
Justine Ruch <justinegabrielleruch@gmail.com>
2025-08-06 13:52:54 -0400

    changed PRauc variable N in the function

--- R/PRauc.R
+++ R/PRauc.R
@@ -1,13 +1,5 @@
 PR_AUC <- function(pred_tensor, label_tensor){
   pr = PR_curve(pred_tensor, label_tensor)
-  recall_diff = pr$recall[3:N]-pr$recall[2:-2]
-  precision_sum = pr$precision[3:N]+pr$precision[2:-2]
-  torch::torch_sum(recall_diff*precision_sum/2.0)
-}
-
-
-PR_AUC <- function(pred_tensor, label_tensor){
-  pr = PR_curve(pred_tensor, label_tensor)
   N = length(pr$recall)  
   recall_diff = pr$recall[3:N]-pr$recall[2:-2]
   precision_sum = pr$precision[3:N]+pr$precision[2:-2]
